d6e21fa827fc9f3a86c159c4d144180e06886ec6
Aahil Alwani <alwani.aahil25@gmail.com>
2025-03-10 16:28:45 +0500

    feat: add schemas for validation

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,17 +1,20 @@
 //all routes will be gathered here
-const userController = require('../controllers/userController')
+const userController = require('../controllers/userController');
+const { signupSchema, loginSchema } = require('../schema/authSchema');
 const authValidator= require('../validators/authValidator')
 
 const authRoutes=[
     {
         url:'/register',
         method:'POST',
+        schema: signupSchema,
         handler: userController.registerController,
         preHandler: authValidator.registerValidator,
     },
     {
         url:'/login',
         method:'POST',
+        loginSchema,
         handler: userController.loginController,
         preHandler: authValidator.loginValidator
     }
--- src/schema/authSchema.js
+++ src/schema/authSchema.js
@@ -1,9 +1,24 @@
 const signupSchema={
-    querystring:{
-        
+    body: {
+        type: 'object',
+        required: ['email', 'password', 'name'],
+        properties: {
+            email: { type: 'string', format: 'email' },
+            password: { type: 'string', minLength: 6 },
+            name: { type: 'string', minLength: 2 }
+        }
     }
 }
 
 const loginSchema={
-
+    body: {
+        type: 'object',
+        required: ['email', 'password'],
+        properties: {
+            email: { type: 'string', format: 'email' },
+            password: { type: 'string', minLength: 6 }
+        }
+    }
 }
+
+module.exports={loginSchema,signupSchema}
